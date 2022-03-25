029c37d6543a14cf35da2606968e421e820d2c6c
jaime00 <jtorres6100@gmail.com>
2022-03-25 14:55:22 -0500

    Update new work

--- src/components/Presentation/index.js
+++ src/components/Presentation/index.js
@@ -4,11 +4,11 @@ export default function Presentation() {
 	return (
 		<div className="grid items-center grid-cols-1 mt-12 text-center md:mt-24 md:text-left md:grid-cols-6 mb-5">
 			<h1 className="mx-2 order-2 col-span-5 text-gray-900 dark:text-white font-extrabold text-4xl leading-tight md:leading-normal md:order-1 sm:text-5xl">
-				I'm <span className="text-teal-400 dark:text-teal-300">Jaime</span>. I'm a developer and technical leader working at Extreme Technologies.
+				I'm <span className="text-teal-400 dark:text-teal-300">Jaime</span>. I'm a Frontend Developer working at Qrvey.
 			</h1>
 			<div className="mx-auto order-1 md:order-2">
 				<span>
-					<img loading='lazy' alt="Jaime Torres" src={image_profile} className="col-span-1 rounded-full" />
+					<img loading="lazy" alt="Jaime Torres" src={image_profile} className="col-span-1 rounded-full" />
 				</span>
 			</div>
 		</div>
