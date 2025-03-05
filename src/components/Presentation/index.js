3ccd0a2d0ce0fa741ed7a96bf5cbe05421887df3
jaime00 <jtorres6100@gmail.com>
2025-03-04 23:51:12 -0500

    refactor: Optimize terminal text animation and component styling

--- src/components/Presentation/index.js
+++ src/components/Presentation/index.js
@@ -5,22 +5,19 @@ import terminalText from '../../utils/terminalText';
 export default function Presentation() {
   useEffect(() => {
     terminalText(['React JS.', 'Next JS.', 'Stencil JS.', 'Typescript.', 'Web Components.'], 'text');
-    // terminalText(['Web Components.'], 'text');
   }, []);
   return (
     <div className="grid items-center grid-cols-1 mt-12 text-center md:mt-24 md:text-left md:grid-cols-6 mb-5">
       <div className='col-span-5 mx-2'>
-        <p class="text-gray-400 font-medium mb-5 text-left">
+        <p className="text-gray-400 font-medium mb-5 text-left">
           +5 years of experience in web development
         </p>
         <h1 className="text-left order-2 text-gray-900 dark:text-white font-extrabold text-4xl leading-tight md:order-1 sm:text-5xl">
           I'm <span className="text-teal-400 dark:text-teal-300">Jaime</span>. I'm a
           <span className="text-teal-400 dark:text-teal-300"> Frontend</span> Developer specialized in{" "}
-          <span id='text' className='text-teal-400 dark:text-teal-300'></span>
-          {/* <div class='console-container text-teal-400 dark:text-teal-300 inline'>
-            
-            <span className='invisible'>ssssssssssssssssssssssss</span>
-          </div> */}
+          <div className='console-container inline-block'>
+            <span id='text'></span>
+          </div>
         </h1>
       </div>
       <div className="mx-auto order-1 hidden md:block">
