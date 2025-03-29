6e1f434e045c2e75ccc5ae8463df3506a1993076
jaime00 <jtorres6100@gmail.com>
2025-03-29 14:17:05 -0500

    feat: Upgrade React to version 19, enhance index.html with theme toggle script, and improve button component structure

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -5,7 +5,7 @@ export default function Project(props) {
 	const { url_preview, url_code, title, description, img, stack, id } = props
 	return (
 		<>
-			<div className="wrapper antialiased text-gray-900 min-w-[350px] max-w-[405px]">
+			<div className="wrapper antialiased text-gray-900 min-w-[350px] max-w-[405px] group">
 				<div className='overflow-hidden hover:rounded-lg'>
 					<img
 						lazy="true"
@@ -14,7 +14,7 @@ export default function Project(props) {
 						}}
 						src={img}
 						alt={title}
-						className="w-full object-center rounded-lg shadow-md transition-all duration-700 hover:scale-110 object-cover"
+						className="w-full object-center rounded-lg shadow-md transition-all duration-700 group-hover:scale-110 object-cover"
 					/>
 					<div className="relative px-4 -mt-16">
 						<div className="bg-white dark:bg-gray-900 dark:text-white p-5 rounded-lg shadow-lg h-72 sm:pb-5">
