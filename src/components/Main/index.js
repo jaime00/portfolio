9d68e4856f412c3d504efb535183adcb44899542
jaime00 <jtorres6100@gmail.com>
2025-04-02 00:27:11 -0500

    feat: Update CarouselOfTechnologies to include Svelte, reorder technology icons, and enhance Presentation component with gradient text styling

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -6,7 +6,7 @@ export default function Main({ isDark }) {
   return (
     <main className="h-[83vh] flex flex-col mx-auto max-w-6xl justify-center px-4 bg-white dark:bg-gray-800 prose prose-lg md:prose-xl dark:prose-dark relative font-sans">
       <div className="container mx-auto flex flex-wrap flex-col gap-6">
-        <Presentation />
+        <Presentation isDark={isDark} />
         <div className="my-5 space-y-6 md:space-y-0 md:space-x-4">
           <Button isDark={true} to="/contact">
             Contact me
