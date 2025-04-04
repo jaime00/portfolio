c0bd0acef772beea9d13f918b9ce65776adf61ae
jaime00 <jtorres6100@gmail.com>
2025-04-04 04:29:54 -0500

    feat: Integrate framer-motion for animated icons, add ArrowRightIcon, MoonIcon, SunMoonIcon components, and update ButtonDarkMode for improved dark mode functionality

--- src/components/Bottom/index.js
+++ src/components/Bottom/index.js
@@ -3,8 +3,8 @@ import { ReactComponent as BottomIcon } from '../../assets/icons/bottom.svg'
 export default function Bottom() {
 	const handleClick = () => window.scrollTo({ behavior: 'smooth', top: document.getElementById('work-experience-container').offsetTop })
 	return (
-		<div className="flex justify-center rotate-180 cursor-pointer animate-bounce text-gray-500 hover:text-black dark:hover:text-white" onClick={handleClick}>
-			<BottomIcon />
+		<div className="flex justify-center cursor-pointer animate-bounce text-gray-500" onClick={handleClick}>
+			<BottomIcon className='hover:text-black dark:hover:text-white rotate-180'/>
 		</div>
 	)
 }
