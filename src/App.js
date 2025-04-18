1ce4740f4683434a3f755bc2690611537b41afd0
jaime00 <jtorres6100@gmail.com>
2025-04-17 20:45:12 -0500

    refactor: Remove unused Fragment import from App.js for cleaner code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './styles/output.css';
 import './styles/general.css';
 import { Route, Redirect, Switch } from 'wouter';
