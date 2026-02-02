57ae95b4e698b0bb587abc3ffb6e6d4c63c5393d
jaime00 <jtorres6100@gmail.com>
2026-02-01 20:17:15 -0500

    feat: Configurar Craco para gestionar opciones de compilación de CRA
    Se añade @craco/craco como dependencia de desarrollo para permitir la configuración
    personalizada de Create React App sin necesidad de ejectarlo. Se actualiza package.json
    para utilizar Craco en los scripts de start, build y test. Se crea craco.config.js
    con configuración de middleware para webpack-dev-server. Se añade npx browserslist
    a comandos permitidos y se reformatean estilos CSS.

---
+++ craco.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  devServer: (devServerConfig) => {
+    // Eliminar las opciones obsoletas
+    delete devServerConfig.onBeforeSetupMiddleware;
+    delete devServerConfig.onAfterSetupMiddleware;
+
+    // Usar setupMiddlewares en su lugar
+    devServerConfig.setupMiddlewares = (middlewares, devServer) => {
+      if (!devServer) {
+        throw new Error('webpack-dev-server is not defined');
+      }
+
+      // Aquí puedes agregar middlewares personalizados si los necesitas
+      // middlewares.unshift(...);
+      // middlewares.push(...);
+
+      return middlewares;
+    };
+
+    return devServerConfig;
+  },
+};
