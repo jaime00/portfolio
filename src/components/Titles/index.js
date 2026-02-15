e968293dcfecd41a1b8cd3cca2fb33079e16faa1
jaime00 <jtorres6100@gmail.com>
2026-02-15 01:25:54 -0500

    feat: Add gradient to title text in Titles component

--- src/components/Titles/index.js
+++ src/components/Titles/index.js
@@ -1,7 +1,7 @@
 export default function Titles({ title, subtitle }) {
   return (
     <h1 className="mx-auto mb-5">
-      <span className="block text-center text-base font-semibold uppercase tracking-wide text-teal-500 dark:text-teal-400">
+      <span className="block bg-gradient-to-r from-teal-500 to-teal-400 bg-clip-text text-center text-base font-semibold uppercase tracking-wide text-transparent">
         {title}
       </span>
       <span className="my-6 mx-auto block max-w-2xl text-center text-4xl font-bold leading-10 sm:text-5xl">
