43398a44562ee6ef120ef3738eae81f373ce9fc8
jaime00 <jtorres6100@gmail.com>
2026-06-22 10:50:43 -0500

    style: highlight tech keywords with bold and emphasis in about section

--- src/components/MyHistory/index.js
+++ src/components/MyHistory/index.js
@@ -27,7 +27,7 @@ export default function MyHistory() {
         </p>
         <br />
 
-        <p>{t('about.technologies')}</p>
+        <p dangerouslySetInnerHTML={{ __html: t('about.technologies') }} />
         <div className="mt-5 grid grid-cols-1 gap-0 md:grid-cols-4 md:gap-x-10">
           <div className="col-span-1 self-center">
             <span className="flex justify-center">
