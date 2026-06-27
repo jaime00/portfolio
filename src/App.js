2cae2c7a182614eafcb05a75b99282a619baca85
jaime00 <jtorres6100@gmail.com>
2026-06-27 00:15:04 -0500

    fix: add min-h-screen to prevent layout cutoff on mobile contact page
    Also reorder projects: swap eazy-git and negos positions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Route, Redirect, Switch } from 'wouter'
 import { Toaster } from 'sonner'
+
 import { MotionConfig } from 'motion/react'
 
 import Background from '@/components/Background'
@@ -60,7 +61,7 @@ function App() {
               'dark:!bg-gray-800/80 dark:!text-white dark:!border-teal-400/10'
           }}
         />
-        <div className="pt-2 dark:bg-gray-800">
+        <div className="min-h-screen pt-2 dark:bg-gray-800">
           <a
             href="#main-content"
             className="sr-only focus:not-sr-only focus:fixed focus:left-4 focus:top-4 focus:z-[9999999] focus:rounded-lg focus:bg-teal-500 focus:px-4 focus:py-2 focus:text-white focus:outline-none"
